0a450f6875c7ea9819ee54bba004294d32ddefb4
horsenoiseadministrator <103950749+horsenoiseadministrator@users.noreply.github.com>
2023-06-27 22:53:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,51 +8,8 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Free UK postcode geocoder API</h1>
-<p>Uses a 2021 dataset from <a href='https://www.freemaptools.com/download-uk-postcode-lat-lng.htm'>Freemaptools</a></p><br><br><p>The endpoint is <a href='/api/v1/geocode?postcode=N1C4AX'>/api/v1/geocode?postcode=yourpostcode</a></p>'''
+    return render_template('index.html')
 
 
-
-@app.route('/api/v1/geocode', methods=['GET'])
-def api_id():
-    
-    if 'postcode' in request.args:
-        pcode = request.args['postcode'].upper().replace(" ","")
-    else:
-        return "Error: Please provide a postcode."
-
-    filename = 'data_part_1.csv'
-
-    with open(filename, 'r') as csvfile:
-        datareader = csv.reader(csvfile)
-        for row in datareader:
-            if row[1] == pcode:
-                rehults = row[2] + ',' + row[3]
-                
-    csvfile.close
-
-
-    filename = 'data_part_2.csv'
-
-    with open(filename, 'r') as csvfile:
-        datareader = csv.reader(csvfile)
-        for row in datareader:
-            if row[1] == pcode:
-                rehults = row[2] + ',' + row[3]
-                
-    csvfile.close
-
-
-    filename = 'data_part_3.csv'
-
-    with open(filename, 'r') as csvfile:
-        datareader = csv.reader(csvfile)
-        for row in datareader:
-            if row[1] == pcode:
-                rehults = row[2] + ',' + row[3]
-                
-    csvfile.close
-
-    return rehults
 
 app.run()
